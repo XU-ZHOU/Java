1337cccc0387337dd7ac0b029784f694ddf23171
XU-ZHOU <34745915+XU-ZHOU@users.noreply.github.com>
2019-05-06 22:35:14 +0800

    Add files via upload
    互联网大厂高频重点面试题第2季(上)
    课程笔记：思维导图+Demo代码

---
+++ src/HelloGC.java
@@ -0,0 +1,6 @@
+public class HelloGC {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("......Hello GC");
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
