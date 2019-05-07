9e4b5e01d40b75a70f402bfa2a271822cf102fbd
XU-ZHOU <34745915+XU-ZHOU@users.noreply.github.com>
2019-05-07 09:11:24 +0800

    Delete HelloGC.java

--- src/HelloGC.java
+++
@@ -1,6 +0,0 @@
-public class HelloGC {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("......Hello GC");
-        Thread.sleep(Integer.MAX_VALUE);
-    }
-}
