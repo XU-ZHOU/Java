722f7ced103904fedcfdeeb6c55c7f445abdebb7
XU-ZHOU <34745915+XU-ZHOU@users.noreply.github.com>
2019-05-09 16:32:12 +0800

    Add files via upload

--- 尚硅谷-互联网大厂高频重点面试题第2季(上)-思维导图+Demo代码/Demo代码/HelloGC.java
+++ 尚硅谷-互联网大厂高频重点面试题第2季(上)-思维导图+Demo代码/Demo代码/HelloGC.java
@@ -1,6 +1,9 @@
 public class HelloGC {
+    public static int oneAddone(int x,int y){
+        return x+y;
+    }
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("......Hello GC");
-        Thread.sleep(Integer.MAX_VALUE);
+        int res = oneAddone(1,1);
+        System.out.println(res);
     }
 }
